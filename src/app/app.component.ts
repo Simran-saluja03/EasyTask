7cc2075b197c9a4e6955de1efa0aed9af88d872b
Simran Saluja <simransaluja03@gmail.com>
2025-12-12 02:28:20 +0530

    header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root', //something which other components use to identify this component.
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent], //whatever components are used inside this component. (in html)
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
