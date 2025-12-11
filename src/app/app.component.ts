a741fe2d56c7bd8ae3cd5edf27959b126d928a85
Simran Saluja <simransaluja03@gmail.com>
2025-12-12 02:42:13 +0530

    user component and user data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root', //something which other components use to identify this component.
   standalone: true,
-  imports: [HeaderComponent], //whatever components are used inside this component. (in html)
+  imports: [HeaderComponent, UserComponent], //whatever components are used inside this component. (in html)
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
